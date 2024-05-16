c15a52a31cb59003d2681fb7f801d22510b5fda9
Anshika <97749793+Anshika-sona@users.noreply.github.com>
2024-05-16 08:08:17 +0530

    Create UserController.java

---
+++ UserController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.User;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/create_user")
+    public String createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+}
